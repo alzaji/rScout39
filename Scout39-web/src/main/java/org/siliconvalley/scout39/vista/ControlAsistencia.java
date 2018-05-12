7160bf4b40b1dc6525369c52452cd5b7521c2867
Hidden-Process <pablobellido@uma.es>
2018-05-13 01:30:04 +0200

    Actualizada Backin bean ControlAsistencia actualizada

--- Scout39-web/src/main/java/org/siliconvalley/scout39/vista/ControlAsistencia.java
+++ Scout39-web/src/main/java/org/siliconvalley/scout39/vista/ControlAsistencia.java
@@ -9,7 +9,6 @@ import org.siliconvalley.scout39.modelo.*;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,46 +30,103 @@ public class ControlAsistencia implements Serializable{
     @Inject
     private ControlAutorizacion control;
     
+    @Inject
+    private beanEventos evento;
+    
     public ControlAsistencia(){
       eventosKIM = new HashMap<>();
       eventosSIRYU = new HashMap<>();
       eventosTHA = new HashMap<>();
       eventosALMOGAMA = new HashMap<>();
-    
+              
     }
     
-    public String AsistirEvento(Eventos i){
+    private Progresion crearProgresion(Long idEvento,Long idUsuario){
+        Progresion p = new Progresion();
+        p.setIdProgresion(crearIdProgresion(idEvento, idUsuario));
+        p.setAnimacion(0);
+        p.setIntegracion(0);
+        p.setParticipacion(0);
+        p.setPromesa("");
+        return p;
+    }
+    
+    private ProgresionUsuarioEventosDebil crearIdProgresion(Long idEvento, Long idUsuario){
+        ProgresionUsuarioEventosDebil p = new ProgresionUsuarioEventosDebil();
+        p.setIdEventos(idEvento);
+        p.setIdUsuario(idUsuario);
+        return p;
+    }
+    
+    public String asistirEvento(Eventos i){
         
-        List<Progresion> p = new ArrayList<>();
+       Progresion p = crearProgresion(i.getId(),control.getUsuario().getId());
         
         switch(control.getUsuario().getRoles().getNombrerol()){
-            case "EducandoKIM" : eventosKIM.put(i, p);
+            case "EducandoKIM" : eventosKIM.get(i).add(p);
             return "kim.xhtml";
-            case "EducandoSIRYU" : eventosSIRYU.put(i,p);
+            case "EducandoSIRYU" : eventosSIRYU.get(i).add(p);
             return "siryu.xhtml";
-            case "EducandoTHA" : eventosTHA.put(i, p);
+            case "EducandoTHA" : eventosTHA.get(i).add(p);
             return "tha.xhtml";
-            case "EducandoALMOGAMA" : eventosALMOGAMA.put(i, p);
+            case "EducandoALMOGAMA" : eventosALMOGAMA.get(i).add(p);
+            return "almogama.xhtml";
+            default : return "index.xhtml";
+        }      
+    }
+    
+    public String noAsistir(Eventos i){
+        
+        Progresion p = crearProgresion(i.getId(),control.getUsuario().getId());
+        
+        switch(control.getUsuario().getRoles().getNombrerol()){
+            case "EducandoKIM" : eventosKIM.get(i).remove(p);
+            return "kim.xhtml";
+            case "EducandoSIRYU" : eventosSIRYU.get(i).remove(p);
+            return "siryu.xhtml";
+            case "EducandoTHA" : eventosTHA.get(i).remove(p);
+            return "tha.xhtml";
+            case "EducandoALMOGAMA" : eventosALMOGAMA.get(i).remove(p);
             return "almogama.xhtml";
             default : return "index.xhtml";
         }
-            
     }
     
-    public String NoAsistir(Eventos i){
+    
+    
+    
+    public boolean comprobarAsistencia(Eventos i){
+        
+        Progresion p = crearProgresion(i.getId(),control.getUsuario().getId());
         
         switch(control.getUsuario().getRoles().getNombrerol()){
-            case "EducandoKIM" : eventosKIM.remove(i);
-            return "kim.xhtml";
-            case "EducandoSIRYU" : eventosSIRYU.remove(i);
-            return "siryu.xhtml";
-            case "EducandoTHA" : eventosTHA.remove(i);
-            return "tha.xhtml";
-            case "EducandoALMOGAMA" : eventosALMOGAMA.remove(i);
-            return "almogama.xhtml";
-            default : return "index.xhtml";
-        }
+            case "EducandoKIM" : 
+                return eventosKIM.get(i).contains(p);
+ 
+            case "EducandoSIRYU" : 
+                return eventosSIRYU.get(i).contains(p);
+                  
+            case "EducandoTHA" :
+                return eventosTHA.get(i).contains(p);
+             
+            case "EducandoALMOGAMA" : 
+                return eventosALMOGAMA.get(i).contains(p);
+                
+            default : return false;  
+        }   
     }
+    
+    
+
+    public beanEventos getEvento() {
+        return evento;
+    }
+
+    public void setEvento(beanEventos evento) {
+        this.evento = evento;
+    }
+    
+    
 
     public ControlAutorizacion getControl() {
         return control;
