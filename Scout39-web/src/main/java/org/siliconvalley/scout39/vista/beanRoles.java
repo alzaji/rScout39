9ab425bfcdb981c773c6658c7364ab64f5bcfc72
Hidden-Process <pablobellido@uma.es>
2018-05-13 16:14:04 +0200

    Bean Roles actualizada

--- Scout39-web/src/main/java/org/siliconvalley/scout39/vista/beanRoles.java
+++ Scout39-web/src/main/java/org/siliconvalley/scout39/vista/beanRoles.java
@@ -7,30 +7,71 @@ package org.siliconvalley.scout39.vista;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
-import javax.enterprise.context.RequestScoped;
+import java.util.Map;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
-import org.siliconvalley.scout39.modelo.Roles;
-import org.siliconvalley.scout39.modelo.Usuario;
+import org.siliconvalley.scout39.modelo.*;
 
 /**
  *
  * @author Fernandez
  */
 @Named(value = "beanRoles")
-@RequestScoped
+@SessionScoped
 public class beanRoles implements Serializable {
 
     private String nombreRol;
-    @Inject
-    private Login login;
+    private Map<Roles, List<Privilegios>> roles;
 
     public beanRoles() {
+        roles = new HashMap<>();
+    }
 
+    private Roles crearRol(Long idRol, String nombreRol) {
+        Roles r = new Roles();
+        r.setIdrol(idRol);
+        r.setNombrerol(nombreRol);
+        return r;
+    }
+
+    private Privilegios crearPrivilegios(Long id) {
+        Privilegios p = new Privilegios();
+        p.setId(id);
+        p.setLectura('N');
+        p.setBorrado('N');
+        p.setEscritura('N');
+        return p;
+    }
+
+    public void añadirNuevoRol(Roles rol) {
+
+        Roles r = crearRol(rol.getId(), rol.getNombrerol());
+
+        FacesContext ctx = FacesContext.getCurrentInstance();
+
+        if (!roles.containsKey(r)) {
+            roles.put(r, new ArrayList<Privilegios>());
+        } else {
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "El rol " + rol.getNombrerol() + " ya se encuentra en la lista de roles", "El rol " + rol.getNombrerol() + " ya se encuentra en la lista de roles"));
+        }
+
+    }
+
+    public void eliminarRole(Roles rol) {
+
+        Roles r = crearRol(rol.getId(), rol.getNombrerol());
+
+        FacesContext ctx = FacesContext.getCurrentInstance();
+
+        if (roles.containsKey(r)) {
+            roles.remove(r);
+        } else {
+            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "El rol " + rol.getNombrerol() + " no se encuentra en la lista de roles", "El rol " + rol.getNombrerol() + " no se encuentra en la lista de roles"));
+        }
     }
 
     public String getNombreRol() {
@@ -39,23 +80,5 @@ public class beanRoles implements Serializable {
 
     public void setNombreRol(String nombreRol) {
         this.nombreRol = nombreRol;
-    }
-
-    public void crearNuevoRole(Roles rol) {
-
-        if (!login.getRoles().add(rol)) {
-            FacesContext ctx = FacesContext.getCurrentInstance();
-            ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "El rol " + rol.getNombrerol() + " no se encuentra en la lista de roles", "El rol " + rol.getNombrerol() + " no se encuentra en la lista de roles"));
-        }
-    }
-
-    public void eliminarRole(Roles rol) {
-        if (login.getRoles().contains(rol)) {
-            for (Usuario u : login.getUsuarios()) {
-                if (u.getRoles().equals(rol)) {
-                    u.setRoles(login.getRoles().get(1));
-                }
-            }
-        }
     }
 }
