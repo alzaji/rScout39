bf61d2bc9c3c184477462f672568fd78c28de33c
Antonio Ruiz de la Torre <aruizdlt@uma.es>
2018-05-13 02:23:33 +0200

    Modificar Evento implementado

--- Scout39-web/src/main/java/org/siliconvalley/scout39/vista/beanEventos.java
+++ Scout39-web/src/main/java/org/siliconvalley/scout39/vista/beanEventos.java
@@ -11,9 +11,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.enterprise.context.SessionScoped;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import org.siliconvalley.scout39.modelo.*;
 
 /**
@@ -21,7 +23,8 @@ import org.siliconvalley.scout39.modelo.*;
  * @author aruizdlt
  */
 @Named(value = "beanEventos")
-@SessionScoped
+@ManagedBean
+@RequestScoped
 public class beanEventos implements Serializable {
 
     private long idEvento = 1;
@@ -37,10 +40,16 @@ public class beanEventos implements Serializable {
     public beanEventos() {
         eventosTHA = new ArrayList<>();
         eventosSIRYU = new ArrayList<>();
-        eventosSIRYU.add(crearEvento("Evento1", "Este es el evento de prueba", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
-        eventosSIRYU.add(crearEvento("Evento2", "Este es el evento de prueba2", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
         eventosKIM = new ArrayList<>();
         eventosALMOGAMA = new ArrayList<>();
+        eventosSIRYU.add(crearEvento("Evento1", "Este es el evento de prueba", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosSIRYU.add(crearEvento("Evento2", "Este es el evento de prueba2", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));       
+        eventosTHA.add(crearEvento("Evento1", "Este es el evento de prueba", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosTHA.add(crearEvento("Evento2", "Este es el evento de prueba2", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosKIM.add(crearEvento("Evento1", "Este es el evento de prueba", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosKIM.add(crearEvento("Evento2", "Este es el evento de prueba2", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosALMOGAMA.add(crearEvento("Evento1", "Este es el evento de prueba", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
+        eventosALMOGAMA.add(crearEvento("Evento2", "Este es el evento de prueba2", new Date(), new BigDecimal(36.7147093), new BigDecimal(-4.4757148)));
     }
 
     public List<Eventos> misEventos() {
@@ -67,24 +76,67 @@ public class beanEventos implements Serializable {
         }
     }
 
-    public String doCrearEvento(String nombre, String descripcion, String fecha, String latitud, String longitud) {
-        Eventos evento = crearEvento(nombre, descripcion, new Date(fecha), new BigDecimal(latitud), new BigDecimal(longitud));
+    public void doCrearEvento() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String nombre = request.getParameter("formCrearEvento:crearNombre");
+        String descripcion = request.getParameter("formCrearEvento:crearDescripcion");
+        //String fecha = request.getParameter("formCrearEvento:crearFecha");
+        String latitud = request.getParameter("formCrearEvento:crearLatitud");
+        String longitud = request.getParameter("formCrearEvento:crearLongitud");
+        Eventos evento = crearEvento(nombre, descripcion, new Date(), new BigDecimal(latitud), new BigDecimal(longitud));
         switch (control.getUsuario().getRoles().getNombrerol()) {
             case "ScouterTHA":
                 eventosTHA.add(evento);
-                return "tha.xhtml";
+                break;
             case "ScouterKIM":
                 eventosKIM.add(evento);
-                return "kim.xhtml";
+                break;
             case "ScouterSIRYU":
                 eventosSIRYU.add(evento);
-                return "siryu.xhtml";
+                break;
             case "ScouterALMOGAMA":
                 eventosALMOGAMA.add(evento);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public String doModificarEvento(String idEvento) {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String nombre = request.getParameter("formModificarEvento" + idEvento + ":modificarNombre");
+        String descripcion = request.getParameter("formModificarEvento" + idEvento + ":modificarDescripcion");
+        //String fecha = request.getParameter("formModificarEvento" + idEvento + ":modificarFecha");
+        //String latitud = request.getParameter("formModificarEvento" + idEvento + ":modificarLatitud");
+        //String longitud = request.getParameter("formModificarEvento" + idEvento + ":modificarLongitud");
+        Eventos evento = crearEventoId(idEvento, nombre, descripcion, new Date(), new BigDecimal(36.7147093), new BigDecimal(36.7147093));
+        switch (control.getUsuario().getRoles().getNombrerol()) {
+            case "ScouterTHA":
+                modificarEvento(eventosTHA, evento);
+                return "tha.xhtml";
+            case "ScouterKIM":
+                modificarEvento(eventosKIM, evento);
+                return "kim.xhtml";
+            case "ScouterSIRYU":
+                modificarEvento(eventosSIRYU, evento);
+                return "siryu.xhtml";
+            case "ScouterALMOGAMA":
+                modificarEvento(eventosALMOGAMA, evento);
                 return "almogama.xhtml";
             default:
                 return "index.xhtml";
-        }        
+        }
+    }
+
+    private Eventos crearEventoId(String id, String nombre, String descripcion, Date fecha, BigDecimal latitud, BigDecimal longitud) {
+        Eventos evento = new Eventos();
+        evento.setNombre(nombre);
+        evento.setId(new Long(id));
+        evento.setDescripcion(descripcion);
+        evento.setFecha(fecha);
+        evento.setLatitud(latitud);
+        evento.setLongitud(longitud);
+        return evento;
     }
 
     private Eventos crearEvento(String nombre, String descripcion, Date fecha, BigDecimal latitud, BigDecimal longitud) {
@@ -98,7 +150,12 @@ public class beanEventos implements Serializable {
         evento.setLongitud(longitud);
         return evento;
     }
-
+    
+    private void modificarEvento(List<Eventos> eventos, Eventos e){
+        int posicion = eventos.indexOf(e);
+        eventos.toArray()[posicion] = e;
+    }
+         
     public Eventos getEvento() {
         return evento;
     }
